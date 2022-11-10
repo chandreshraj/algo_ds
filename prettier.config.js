166094a858d973c6f14b33e0ab424e41309e958b
chandresh raju <chandresh.raju@ubermetrics-technologies.com>
2022-11-10 16:14:45 +0100

    Linear Search

---
+++ prettier.config.js
@@ -0,0 +1,6 @@
+export const tabWidth = 4;
+export const printWidth = 80;
+export const proseWrap = "never";
+export const trailingComma = "all";
+export const singleQuote = false;
+export const semi = true;
---
+++ src/LinearSearch/linearSearch.ts
@@ -0,0 +1,8 @@
+function linearSearch(input:number[], searchElement:number):boolean{
+    for(let i=0;i<input.length;i++){
+        if(input[i] === searchElement) return true;
+    }
+    return false;
+}
+
+export default linearSearch;
