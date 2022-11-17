dfc5832be54a0b39afa0f9a6efa30bfdf6b6d4c6
chandresh raju <chandresh.raju@ubermetrics-technologies.com>
2022-11-17 14:02:04 +0100

    Binary Search

--- src/BinaySearch/binarySearch.ts
+++ src/BinaySearch/binarySearch.ts
@@ -1,5 +1,16 @@
-const binarySearch = (input: number[], searchElement: number): Number => {
-    return -1;
+const binarySearch = (input: number[], searchElement: number): boolean => {
+    let lo = 0;
+    let hi = input.length;
+
+    do {
+        let m = Math.floor(lo + (hi - lo) / 2);
+        const v = input[m];
+        if (v == searchElement) return true;
+        else if (v > searchElement) hi = m;
+        else lo = m + 1;
+    } while (lo < hi);
+
+    return false;
 };
 
 export default binarySearch;
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,17 @@
 import linearSearch from './LinearSearch/linearSearch';
+import binarySearch from './BinaySearch/binarySearch';
 
 const input = [1, 2, 3, 4, 5, 6];
-const searchKey = 2;
+const searchKey = 10;
 
 console.log(
     `Linear Search of ${searchKey} -> ${
         linearSearch(input, searchKey) ? 'Found' : 'Not Found'
     }`,
+);
+
+console.log(
+    `Binary Search of ${searchKey} -> ${
+        binarySearch(input,searchKey) ? 'Found' : 'Not Found'
+    }`
 );
